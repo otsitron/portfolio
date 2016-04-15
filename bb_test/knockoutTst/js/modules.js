a5fe945d8ddab35c01886c3b0ac2e25b34b49fa5
Olga Tsitron <XBBL4SV@IPMS.COM>
2016-04-15 16:23:34 -0400

    requirejs test

--- bb_test/knockoutTst/js/modules.js
+++ bb_test/knockoutTst/js/modules.js
@@ -1,13 +1,13 @@
 requirejs.config({
-    baseUrl: '../libs',
+    baseUrl: 'js',
     paths: {
-        app: '../knockoutTst/js'
+        app: 'js/libs'
     }
 });
 
-requirejs([ 'jquery-1.12.3.min', 
-            'knockout-3.4.0', 
-            'bootstrap-3.3.6-dist/js/bootstrap',
-            'app/logic'], function(util) {
+requirejs([ 'libs/jquery-1.12.3.min', 
+            'libs/knockout-3.4.0', 
+            'libs/bootstrap-3.3.6-dist/js/bootstrap.min',
+            'logic'], function(util) {
                 console.log("asdfasdf");
 });
